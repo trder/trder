a1db30876092498176d2747d66a2039505065b20
trder <103327499+trder@users.noreply.github.com>
2022-05-02 17:28:40 +0800

    方法定义

--- trder_core.py
+++ trder_core.py
@@ -1,1 +1,12 @@
 
+def load_trading_system(trading_system_name):
+    '''
+    载入交易系统
+    '''
+    return None
+  
+def eval_trading_system(trading_system):
+    '''
+    评估交易系统
+    '''
+    return None
