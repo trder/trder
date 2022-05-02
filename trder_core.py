3d7d05f8375467801b2828738aa9e6d1d9a9cc58
trder <103327499+trder@users.noreply.github.com>
2022-05-02 17:52:38 +0800

    Update trder_core.py

--- trder_core.py
+++ trder_core.py
@@ -1,12 +1,15 @@
-
 def load_trading_system(trading_system_name):
     '''
     载入交易系统
     '''
+    if not trading_system_name:
+        return None
     return None
-  
+    
 def eval_trading_system(trading_system):
     '''
     评估交易系统
     '''
+    if not trading_system:
+        return None
     return None
