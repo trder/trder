339ba4ab470e185fed89a63258098e5ae015b89a
trder <103327499+trder@users.noreply.github.com>
2022-05-02 17:20:03 +0800

    from trder_core import *

--- eval.py
+++ eval.py
@@ -2,13 +2,13 @@
 #启动模拟交易
 #指令：python eval.py donchian
 import sys
-from trder import *
+from trder_core import *
 
 if __name__ == '__main__':
     #获取参数个数
     nargv = len(sys.argv)
     if nargv < 2:
-        print("请输入交易系统名！\n正确的指令格式：\npython eval.py 交易系统名")
+        print("请输入交易系统名！\n正确的指令格式为：\npython eval.py 交易系统名")
         return
     #获取交易系统名
     trading_system_name = sys.argv[1]
