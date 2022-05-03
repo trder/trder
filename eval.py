7c8ce28b4bdec273ad973817518c9a5b989e3c96
Boris Town <boristown@gmail.com>
2022-05-03 14:55:21 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -9,18 +9,18 @@ if __name__ == '__main__':
     nargv = len(sys.argv)
     if nargv < 2:
         print("请输入交易系统名！\n正确的指令格式为：\npython eval.py 交易系统名")
-        return
-    #获取交易系统名
-    trading_system_name = sys.argv[1]
-    #载入交易系统
-    trading_system = load_trading_system(trading_system_name)
-    if not trading_system:
-        print("交易系统载入失败！")
-        return
-    #评估交易系统
-    eval_result = eval_trading_system(trading_system)
-    if not eval_result:
-        print("出现异常，评估失败！")
-        return
-    #输出评估结果
-    print(eval_result)
+    else:
+        #获取交易系统名
+        trading_system_name = sys.argv[1]
+        #载入交易系统
+        trading_system = load_trading_system(trading_system_name)
+        if not trading_system:
+            print("交易系统载入失败！")
+        else:
+            #评估交易系统
+            eval_result = eval_trading_system(trading_system)
+            if not eval_result:
+                print("出现异常，评估失败！")
+            else:
+                #输出评估结果
+                print(eval_result)
