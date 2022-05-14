d64b5295c2323a04b60296f0d4b601687d6b9456
Boris Town <boristown@gmail.com>
2022-05-14 12:27:16 +0800

    Update trder.py

--- trder.py
+++ trder.py
@@ -6,13 +6,13 @@ D_MA350D = {}
 D_DON10D_BREAK = {}
 D_DON20D_BREAK = {}
 
-def ATRP20D(exchange,symbol):
+def get_ATRP20D(exchange,symbol):
     return D_ATRP20D[(exchange,symbol)]
 
-def MA25D(exchange,symbol):
+def get_MA25D(exchange,symbol):
     return D_MA25D[(exchange,symbol)]
 
-def MA350D(exchange,symbol):
+def get_MA350D(exchange,symbol):
     return D_MA350D[(exchange,symbol)]
 
 def set_ATRP20D(exchange,symbol,val):
@@ -24,13 +24,13 @@ def set_MA25D(exchange,symbol,val):
 def set_MA350D(exchange,symbol,val):
     D_MA350D[(exchange,symbol)] = val
 
-def DON10D_BREAK(exchange,symbol):
+def get_DON10D_BREAK(exchange,symbol):
     '''
     是否突破唐奇安通道？（0未突破；1向上突破；-1向下突破）
     '''
     return D_DON10D_BREAK[(exchange,symbol)]
 
-def DON20D_BREAK(exchange,symbol):
+def get_DON20D_BREAK(exchange,symbol):
     '''
     是否突破唐奇安通道？（0未突破；1向上突破；-1向下突破）
     '''
@@ -48,6 +48,12 @@ def set_DON20D_BREAK(exchange,symbol):
     '''
     return False
 
+def get_TOTAL_POS():
+    return TOTAL_POS
+
+def get_MARGIN():
+    return MARGIN
+
 def set_TOTAL_POS(val):
     global TOTAL_POS
     TOTAL_POS = val
@@ -55,15 +61,3 @@ def set_TOTAL_POS(val):
 def set_MARGIN(val):
     global MARGIN
     MARGIN = val
-
-def set_ATRP20D(val):
-    global ATRP20D
-    ATRP20D = val
-    
-def set_MA25D(val):
-    global MA25D
-    MA25D = val
-
-def set_MA350D(val):
-    global MA350D
-    MA350D = val
