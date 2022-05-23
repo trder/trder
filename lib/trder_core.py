9369c266ce7917b9ef02e3362b56ef7307fd262a
Boris Town <boristown@gmail.com>
2022-05-23 21:40:05 +0800

    优化输出

--- lib/trder_core.py
+++ lib/trder_core.py
@@ -9,9 +9,9 @@ import time
 def check_trading_system(trading_system_name,param):
     '''
     检查交易系统
-    输入：
+    输入:
     trading_system_name:交易系统名
-    输出：
+    输出:
     code: 200成功 其他code均为失败
     msg: 消息
     '''
@@ -19,7 +19,7 @@ def check_trading_system(trading_system_name,param):
         return 400, "交易系统名不能为空！"
     dir_name = "trade_"+trading_system_name
     if not dir_exist(dir_name):
-        return 400, "交易系统‘"+trading_system_name+"’不存在！"
+        return 400, "交易系统'"+trading_system_name+"'不存在！"
     trading_system_path = dir_name+"/trading.py"
     if not file_exist(trading_system_path):
         return 400, "文件"+trading_system_path+"不存在！"
@@ -28,7 +28,7 @@ def check_trading_system(trading_system_name,param):
         return 400, "方法"+trading_lib_name+".entry_signal不存在！"
     if not func_exist(trading_lib_name,["trading","exit_signal"]):
         return 400, "方法"+trading_lib_name+".exit_signal不存在！"
-    return 200, "交易系统‘"+trading_system_name+"’载入成功！"
+    return 200, "交易系统'"+trading_system_name+"'载入成功！"
     
 def eval_trading_system(trading_system_name,param):
     '''
@@ -39,10 +39,10 @@ def eval_trading_system(trading_system_name,param):
     symbol = str(param['-symbol']) if '-symbol' in param else "BTC/USDT"
     init_balance = 1000.0
     tm = datetime.datetime.now()
-    print_log("模拟交易：交易所" + exchange + "；市场" + symbol,"I")
-    print_log("初始余额"+str(init_balance)+"，开始时间"+stamp_to_date(since),"I")
-    print_log("系统时间："+str(tm),"I")
+    print_log("模拟交易:交易所" + exchange + ";市场" + symbol,"I")
+    print_log("初始余额:"+str(init_balance)+",开始时间"+stamp_to_date(since),"I")
+    print_log("系统时间:"+str(tm),"I")
     final_balance, last_ts = simulate_trading_single(trading_system_name, exchange, symbol, init_balance, since, param)
     tm = datetime.datetime.now()
-    print_log("系统时间："+str(tm),"I")
-    return 200,"最终余额："+str(final_balance)+"，结束时间："+stamp_to_date(last_ts)
+    print_log("系统时间:"+str(tm),"I")
+    return 200,"最终余额:"+str(final_balance)+",结束时间:"+stamp_to_date(last_ts)
