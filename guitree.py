2a8b83ccc0e06a5c7e9ee2b98d72f2042b1be4a9
Boris Town <boristown@gmail.com>
2022-08-13 18:07:22 +0800

    UI升级

--- guitree.py
+++ guitree.py
@@ -1,27 +1,73 @@
-#量化平台图形界面树
+# 量化平台图形界面树
 from ctypes import alignment
 import PySimpleGUI as sg
+import webbrowser
 
 class guitree:
-    def __init__(self,title,tree1,tree2):
+    def __init__(self, title, tree1, tree2):
         self.title = title
         self.tree1 = tree1
         self.tree2 = tree2
 
     @staticmethod
     def elesize():
-        return (30,4)
+        return (20, 3)
+
+    @staticmethod
+    def elesize_icon():
+        return (30, 30)
+
+    @staticmethod
+    def elesize_back():
+        return (20, 1)
 
     @staticmethod
     def BTN(title):
-        return sg.Button(title,size=guitree.elesize()) 
+        return sg.Button(
+            title,
+            size=guitree.elesize(),
+            font=('楷体', 20)
+        )
+
+    @staticmethod
+    def BTN_BACK(title):
+        return sg.Button(
+            title,
+            size=guitree.elesize_back(),
+            font=('楷体', 20)
+        )
 
     @staticmethod
     def LAB(title):
-        return sg.Text(title,size=guitree.elesize(),justification='center') 
+        return sg.Button(
+            title,
+            size=guitree.elesize(),
+            disabled=True,
+            font=('楷体', 20)
+        )
 
-    def show(self):
+    @staticmethod
+    def github():
+        return guitree.center(sg.Image(
+            # source="img\github_PNG40.png",
+            source="img\github_PNG_60.png", #png图像调整www.iloveimg.com
+            key="github",
+            #background_color="black",
+            visible=True,
+            enable_events=True
+            # size=guitree.elesize_icon()
+        ))
+
+    @staticmethod
+    def center(ele):
+        return sg.Column([[ele]], justification='center', vertical_alignment="center")
+
+    def show(self, parent=None):
+        self.parent = parent
         sg.theme('DarkAmber')   # Add a touch of color
+        # sg.theme('DarkRed')   # Add a touch of color
+        # sg.theme('Black')   # Add a touch of color
+        # sg.theme('DarkBlack')   # Add a touch of color
         # All the stuff inside your window.
         if self.tree1:
             ele1 = guitree.BTN(self.tree1.title)
@@ -31,19 +77,39 @@ class guitree:
             ele2 = guitree.BTN(self.tree2.title)
         else:
             ele2 = guitree.LAB("选项2：未定义")
-        layout = [  [ele1],
-                    [ele2] ]
+        github = guitree.github()
+        if parent:
+            parent_title = "<< "+parent.title
+            ele0 = guitree.BTN_BACK(parent_title)
+            layout = [
+                [ele0],
+                [ele1],
+                [ele2],
+                [github]
+            ]
+        else:
+            parent_title = "<<"
+            layout = [
+                [ele1],
+                [ele2],
+                [github]
+            ]
 
         # Create the Window
-        window = sg.Window(self.title, layout, resizable=True, finalize=True)
+        window = sg.Window(self.title, layout, resizable=False, finalize=True)
 
         while True:
-            event,values = window.read()
+            event, values = window.read()
             if event == sg.WIN_CLOSED:
                 break
             if self.tree1 and event == self.tree1.title:
-                self.tree1.show()
+                window.close()
+                self.tree1.show(self)
             elif self.tree2 and event == self.tree2.title:
-                self.tree2.show()
-
-        window.close()
+                window.close()
+                self.tree2.show(self)
+            elif parent and event == parent_title:
+                window.close()
+                self.parent.show(self.parent.parent)
+            elif event == 'github':
+                webbrowser.open("https://github.com/trder/trder")
